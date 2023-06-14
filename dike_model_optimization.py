0937f90ee49049d7bbe7e2f66b2a0a1c396e31bb
svansanten <suze.vansanten@gmail.com>
2023-06-14 11:57:41 +0200

    Run MOEA 5000 NFE. Start analysis in MORDM_2_generating_alternatives

--- dike_model_optimization.py
+++ dike_model_optimization.py
@@ -6,21 +6,19 @@ from ema_workbench import (
     optimize,
     Scenario,
 )
-from ema_workbench.em_framework.optimization import EpsilonProgress
+from ema_workbench.em_framework.optimization import EpsilonProgress, ArchiveLogger
 from ema_workbench.util import ema_logging
 
 from problem_formulation import get_model_for_problem_formulation
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# in dit bestand verder werken voor de MOEA - hierin reference scenario definieren en resultaten opnieuw schrijven.
-
 if __name__ == "__main__":
     ema_logging.log_to_stderr(ema_logging.INFO)
 
-    model, steps = get_model_for_problem_formulation(2)
+    model, steps = get_model_for_problem_formulation(2) #select right problem formulation
 
-#veranderen naar worst case reference values?
+#Reference values follow from open exploration - no policy applied here
     reference_values = {
         "Bmax": 175,
         "Brate": 1.5,
@@ -45,19 +43,40 @@ if __name__ == "__main__":
 
     convergence_metrics = [EpsilonProgress()]
 
-    espilon = [1e3] * len(model.outcomes)
+    espilon = [1e3] * len(model.outcomes) #standard value, afh. van runtime
 
-    nfe = 200  # proof of principle only, way to low for actual use
+    nfe = 5000  # Set to number that can be seen as converging
 
+
+    #toevoegen van archivelogger to caluclate hypervolume _ opnieuw runnen dus!
+    convergence_metrics = [
+        ArchiveLogger(
+            "./archives",  # important to make a new directory archives to save this information
+            [l.name for l in model.levers],
+            [o.name for o in model.outcomes],
+            base_filename="optimization_nfe5000.tar.gz",
+        ),
+        EpsilonProgress(),
+    ]
+
+    #Perform model evaluation
     with MultiprocessingEvaluator(model) as evaluator:
+        #wat te doen met losse convergence file?
         results, convergence = evaluator.optimize(
             nfe=nfe,
             searchover="levers",
             epsilons=espilon,
             convergence=convergence_metrics,
-            reference=ref_scenario,
+            reference=ref_scenario, #reference scenario is defined
         )
 
+    #save outcomes to csv file
+    results.to_csv('data/output_data/results_optimization_5000nfe.csv')
+    convergence.to_csv('data/output_data/convergence_optimization_5000nfe.csv')
+
+
+
+    #lijkt nu niet nodig?
     fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=True)
     fig, ax1 = plt.subplots(ncols=1)
     ax1.plot(convergence.epsilon_progress)
