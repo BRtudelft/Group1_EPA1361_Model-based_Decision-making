2fa7049a683997faea01f17e133d25e96a29f2d6
svansanten <suze.vansanten@gmail.com>
2023-06-19 08:37:16 +0200

    Run 5000 uncertainty simulations on 9 policies

--- dike_model_uncertainty_simulation.py
+++ dike_model_uncertainty_simulation.py
@@ -23,17 +23,26 @@ if __name__ == "__main__":
 
     model, steps = get_model_for_problem_formulation(2) #select right problem formulation
 
+    #policies includes an unnamed column with index from original dataframe with solutions
     policies = pd.read_csv('data/output_data/policies.csv')
 
-    n_scenarios = 5000
+    n_scenarios = 5000ç
 
     policies_to_evaluate = []
     for i, policy in policies.iterrows():
         policies_to_evaluate.append(Policy(str(i), **policy.to_dict()))
     print(policies_to_evaluate)
 
-    with MultiprocessingEvaluator(model) as evaluator:
-        results = evaluator.perform_experiments(n_scenarios,
-                                            policies_to_evaluate)
+    with MultiprocessingEvaluator(model, n_processes=-1) as evaluator:
+        results = evaluator.perform_experiments(n_scenarios, policies_to_evaluate)
+
+    experiments, outcomes = results
+    # Policy distinction is necessary because of the legend.
+    policies = experiments['policy']
+
+    outcomes_uncertainties = pd.DataFrame.from_dict(outcomes)
+    outcomes_uncertainties['policy'] = policies
+
     #save outcomes to csv file
-    results.to_csv('data/output_data/results_uncertainty_simulation_5000s.csv')
+    experiments.to_csv('data/output_data/experiments_uncertainty_simulation_5000s.csv')
+    outcomes_uncertainties.to_csv('data/output_data/outcomes_uncertainty_simulation_5000s.csv')
